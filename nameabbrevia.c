d833b084f048d91dd196fcf732b19b1b51e26025
jasikasahu <jassika312@gmail.com>
2026-08-15 22:08:32 +0530

    Add full name abbreviation program

---
+++ nameabbrevia.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    char fname[20], mname[20], lname[20]; // first, middle, last name
+
+    printf("Enter full name (first middle last): ");
+    scanf("%19s %19s %19s", fname, mname, lname);
+
+    printf("Abbreviated name: ");
+    printf("%c. %c. %s\n", fname[0], mname[0], lname);
+
+    return 0;
+}
