3b8903c8d0725a074e02c4f7b152a128febf0991
jasikasahu <jassika312@gmail.com>
2026-08-15 21:52:08 +0530

    Add new C program

--- area.c
+++ area.c
@@ -2,10 +2,10 @@
 int main(){
     int radius;
     float pi=3.14,area,circumference;
-    printf("enter the radius of circle");
+    printf("enter the radius of circle =");
     scanf("%d",&radius);
     area=pi*radius*radius;
-    printf("the area of circle is:%F",area);
+    printf("the area of circle is=%F",area);
     circumference=2*pi*radius;
     printf("\n the circumference of circle is=%f",circumference);
     return 0;
