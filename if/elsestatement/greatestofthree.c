1a92cde7c789560447f946d52a801c725f2e0866
jasikasahu <jassika312@gmail.com>
2026-08-18 22:42:37 +0530

    Add greatest of three program

---
+++ if/elsestatement/greatestofthree.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main(){
+    int a,b,c;
+    printf("enter three numbers");
+    scanf("%d" "%d" "%d",&a,&b,&c);
+    if(a>=b&&b>=c){
+        printf("%d is greatest number",a);
+    }
+    else if(b>=a&&b>=c){
+          printf("%d is greatest number",b);
+    }
+    else{
+         printf("%d is greatest number",c); 
+    }
+  
+}
